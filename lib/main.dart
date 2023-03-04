4a54bab7e1ccb89514cd96773e0886095a8353ab
Atilla <atillabarlik@gmail.com>
2023-03-05 00:03:35 +0300

    kronometre sayfası oluşturuldu.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:dgs_rehber/kronometre.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: const Anasayfa(title: 'DGS Puan Hesaplama'),
+      home: FlutterStopWatch(),
     );
   }
 }
